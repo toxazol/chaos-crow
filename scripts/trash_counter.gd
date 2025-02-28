197b576a0c124ebdda8e2a882b4765c8b8ace9dc
toxazol <antonmelni20@gmail.com>
2025-02-28 14:49:53 +0900

    improved trash counter

--- scripts/trash_counter.gd
+++ scripts/trash_counter.gd
@@ -1,6 +1,9 @@
 extends StaticBody2D
 
-@export var trashContactPoints = 1
+@export var pointsForContact = 1
+@export var countUpdateTimeout = 0.3
+
+var countUpdateTimer = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +12,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var contactCount = $TrashDetector.get_overlapping_bodies().size()
-	#print(contactCount)
-	ScoreManager.set_score(contactCount)
+	countUpdateTimer += delta
+	if countUpdateTimer > countUpdateTimeout:
+		var contactCount = countParents($TrashDetector.get_overlapping_bodies())
+		#print(contactCount)
+		ScoreManager.set_score(contactCount * pointsForContact)
+		countUpdateTimer = 0.0
+
+func countParents(nodes: Array[Node2D]):
+	var countedSet := {}
+	for node in nodes:
+		var parent = node.get_parent()
+		if !parent:
+			print_debug("parent not found")
+			continue
+		var id = parent.get_instance_id()
+		countedSet[id] = id
+	return countedSet.size()
